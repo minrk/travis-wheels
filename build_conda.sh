3d36a7a3ad280195a23fb5649bfeffd9e2ef56f4
Phil Elson <pelson.pub@gmail.com>
2014-07-03 15:26:32 +0100

    Proof of concept.

---
+++ build_conda.sh
@@ -0,0 +1,15 @@
+wget http://repo.continuum.io/miniconda/Miniconda-3.4.2-Linux-x86_64.sh -O miniconda.sh;
+
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+conda install conda-build binstar --yes
+
+
+git clone git@github.com:SciTools/conda-recipes-scitools.git
+
+cd conda-recipes-scitools
+
+conda build biggus
+
+# binstar ...
