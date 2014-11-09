e7213bbc4507af7bc3dc235e21a258faa5531ba0
Min RK <benjaminrk@gmail.com>
2014-11-09 10:47:17 -0800

    build Cython on 2.6

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 test -d ${wheelhouse} || mkdir -p ${wheelhouse}
 
-pip wheel -f travis-wheels/wheelhouse --wheel-dir=${wheelhouse} -r requirements.txt
+requirements=requirements.txt
+
+if [[ "${TRAVIS_PYTHON_VERSION}" == "2.6" ]]; then
+  requirements=requirements26.txt
+fi
+
+pip wheel -f travis-wheels/wheelhouse --wheel-dir=${wheelhouse} -r $requirements
 
 ls ${wheelhouse}
 
