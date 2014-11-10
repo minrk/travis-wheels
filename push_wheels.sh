9090f60d8ebe5280cd0c584bb255a3dd8106aba0
Min RK <benjaminrk@gmail.com>
2014-11-09 20:12:28 -0800

    bash typo in push_wheels

--- push_wheels.sh
+++ push_wheels.sh
@@ -6,7 +6,7 @@ export BUILD_BRANCH="builder"
 export PUBLISH_BRANCH="master"
 export REPO="$TRAVIS_REPO_SLUG"
 
-if [ "x$TRAVIS_PULL_REQUEST" == "xfalse" ] && [ "x$TRAVIS_BRANCH" == "x$BUILD_BRANCH" ]; then
+if [[ "x$TRAVIS_PULL_REQUEST" == "xfalse" ]] && [[ "x$TRAVIS_BRANCH" == "x$BUILD_BRANCH" ]]; then
 
     echo "Pushing to ${PUBLISH_BRANCH}."
 
