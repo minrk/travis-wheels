7da0964575b448a30ebbba8e0b3216c2b96f017c
Min RK <benjaminrk@gmail.com>
2014-11-09 20:05:45 -0800

    build Cython on 3.2 (for pyzmq)

--- build_wheels.sh
+++ build_wheels.sh
@@ -6,7 +6,7 @@ test -d ${wheelhouse} || mkdir -p ${wheelhouse}
 
 requirements=requirements.txt
 
-if [[ "${TRAVIS_PYTHON_VERSION}" == "2.6" ]]; then
+if [[ "${TRAVIS_PYTHON_VERSION}" == "2.6" || "${TRAVIS_PYTHON_VERSION}" == "3.2" ]]; then
   requirements=requirements26.txt
 fi
 
